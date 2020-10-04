ac32d642d2d90280a738854df33c61062712f427
Babacang07 <58360419+FaturGans@users.noreply.github.com>
2020-10-04 18:48:57 +0700

    Add files via upload

---
+++ Toolv4.php
@@ -0,0 +1,160 @@
+#usr/bin/bash
+clear
+bi='\33[34;1m' #biru
+ij='\33[32;1m' #ijo
+pr='\33[35;1m' #purple
+cy='\33[36;1m' #cyan
+me='\33[31;1m' #merah
+pu='\33[37;1m' #putih
+ku='\33[33;1m' #kuning
+
+sleep 1
+echo $bi"    
+╭╮╱╭╮╱╱╱╱╭╮╱╱╭╮╱╱╱╱╭━━━━╮╱╱╱╱╭╮╱╱╱╱╭╮╱╭╮
+┃┃╱┃┃╱╱╱╱┃┃╱╭╯╰╮╱╱╱┃╭╮╭╮┃╱╱╱╱┃┃╱╱╱╱┃┃╱┃┃
+┃┃╱┃┣━━┳━╯┣━┻╮╭╋━━╮╰╯┃┃┣┻━┳━━┫┃╱╭╮╭┫╰━╯┃
+┃┃╱┃┃╭╮┃╭╮┃╭╮┃┃┃┃━┫╱╱┃┃┃╭╮┃╭╮┃┃╱┃╰╯┣━━╮┃
+┃╰━╯┃╰╯┃╰╯┃╭╮┃╰┫┃━┫╱╱┃┃┃╰╯┃╰╯┃╰╮╰╮╭╯╱╱┃┃
+╰━━━┫╭━┻━━┻╯╰┻━┻━━╯╱╱╰╯╰━━┻━━┻━╯╱╰╯╱╱╱╰╯
+╱╱╱╱┃┃
+╱╱╱╱╰╯"
+echo
+sleep 1
+echo $ku"#  AUTHOR : Babacang07"
+sleep 1
+echo $pr"#  YT : Bocah Cyber"
+sleep 1
+echo $ku"#  TEAM : PhantomSec1337 "
+echo  $me"============================="
+sleep 1
+echo
+echo $pr"[01] "$ku". No login dark FB"
+sleep 1
+echo $pr"[02] "$ku". BruteMBF FB"
+sleep 1
+echo $pr"[03] "$ku". pishing FB"
+sleep 1
+echo $pr"[04] "$ku". dark premium FB"
+sleep 1
+echo $pr"[05] "$ku". dark FB"
+sleep 1
+echo $pr"[06] "$ku". Sensei Crack Email/FB"
+sleep 1
+echo $pr"[07] "$ku". hacking tolls FB"
+sleep 1
+echo $pr"[08] "$ku". reportF FB"
+sleep 1
+echo $pr"[09] "$ku". crack FB"
+sleep 1
+echo $pr"[10] "$ku". infoData FB"
+sleep 1
+echo $pr"[11] "$ku". autolike FB"
+sleep 1
+echo $pr"[12] "$ku". DarkVip"
+sleep 1
+echo
+      read -p " silahkan pilih beb : " pil :
+#batas gan
+if [ $pil = 1 ]
+then
+git clone https://github.com/FR13ND8/BruteFb
+ls
+cd BruteFb
+ls
+python2 brute.py
+fi
+#batas gan
+if [ $pil = 2 ]
+then
+git clone https://github.com/pirmansx/mbf
+ls
+cd mbf
+ls
+python2 MBF.py
+fi
+#batas gan
+if [ $pil = 3 ]
+then
+git clone https://github.com/Cesar-Hack-Gray/SocialSploit
+ls
+cd SocialSploit
+ls
+bash install.sh
+bash Sploit
+fi
+#batas gan
+if [ $pil = 4 ]
+then
+git clone https://github.com/TheMagizz/DarkPremium
+ls
+cd DarkPremium
+python2 DarkFB.py
+fi
+#batas gan
+if [ $pil = 5 ]
+then
+git clone https://github.com/blackcodercrush/hack-facebook-teman
+ls
+cd hack-facebook-teman
+python2 hack-fb.py
+fi
+#batas gan
+if [ $pil = 6 ]
+then
+git clone https://github.com/BOT-033/Sensei
+ls
+cd Sensei
+python2 main.py
+fi
+#batas gan
+if [ $pil = 7 ]
+then
+git clone https://github.com/DarkCurut08/DarkCurut08
+ls
+cd DarkCurut08
+sh Moreno77.sh
+fi
+#batas gan
+if [ $pil = 8 ]
+then
+git clone https://github.com/IlayTamvan/Report
+ls
+cd Report
+unzip Report.zip
+
+python2 Report.py
+fi
+#batas gan
+if [ $pil = 9 ]
+then
+git clone https://github.com/AMVengeance/FB-React
+ls
+cd FB-React
+bash start
+fi
+#batas gan
+if [ $pil = 10 ]
+then
+git clone https://github.com/FR13ND8/InfoFB
+ls
+cd InfoFB
+ls
+pip2 install -r requirements.txt
+python2 info.py
+fi
+#batas gan
+if [ $pil = 11 ]
+then
+git clone https://github.com/FR13ND8/autolike
+ls
+cd autolike
+php autolike.php
+fi
+#batas gan
+if [ $pil = 12 ]
+then
+git clone https://github.com/Babacang07/DarkVip
+cd DarkVip
+ls
+python2 DarkVip.py
+fi
